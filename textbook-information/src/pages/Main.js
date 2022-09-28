2a23550358a9abe71500bbb77d2e4478a74edd75
RTC-13 <110063185+RTC-13@users.noreply.github.com>
2022-09-28 17:45:01 -0400

    Fix
    Fixing issues

--- textbook-information/src/pages/Main.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { Link } from "react-router-dom"
-export default function Main() {
-    return ( <>
-    <h1>Welcome to Textbook Exchange</h1>
-    <Link to="./TextbookInfo.js">Textbook Item</Link>
-    </>
-    )
-}
