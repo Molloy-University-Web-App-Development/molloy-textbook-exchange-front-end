2f7d5c98b5653171fc3e18c1f3c3c563dab4bac2
RTC-13 <110063185+RTC-13@users.noreply.github.com>
2022-10-26 18:23:30 -0400

    Task

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HashLink } from "react-router-hashlink";
 const Header = () => {
-  return <nav>
-    <ul>
-      <HashLink to="/#home-title">Home</HashLink>
-    </ul>
-  </nav>;
+  return (
+    <nav className="navbar">
+      <ul>
+        <HashLink to="/#home-title">Home</HashLink>
+      </ul>
+    </nav>
+  );
 };
 
 export default Header;
